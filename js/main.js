4c5be36f4dc0166c847faa6b6d4d4613bbdfab0a
kurxz <29847942+kurxz@users.noreply.github.com>
2023-06-15 23:17:14 -0300

    feat: new validations for url

--- js/main.js
+++ js/main.js
@@ -1,62 +1,41 @@
 ytLink.addEventListener("input", function (e) {
-    getURL();
+ if(e.target.value.length < 8) return;
+  getURL();
 });
 
 function getURL() {
-    var url = document.getElementById('ytLink').value;
+  var url = document.getElementById("ytLink").value;
 
-    if (url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/)) {
+  if (
+    url.match(
+      /(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/
+    )
+  ) {
+    var regExp =
+      /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var videoID = url.match(regExp);
 
-        var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-        var videoID = url.match(regExp);
+    if (videoID && videoID[2].length == 11) {
+      let endMax =
+        "https://i3.ytimg.com/vi/" + videoID[2] + "/maxresdefault.jpg";
+      let endHQ = "https://i3.ytimg.com/vi/" + videoID[2] + "/hqdefault.jpg";
 
-        if (videoID && videoID[2].length == 11) {
+      var thumbDiv = document.getElementById("thumbnails");
+      thumbDiv.innerHTML = "";
 
-            let endMax = "https://i3.ytimg.com/vi/" + videoID[2] + "/maxresdefault.jpg"
-            let endHQ = "https://i3.ytimg.com/vi/" + videoID[2] + "/hqdefault.jpg"
-
-            var thumbDiv = document.getElementById("thumbnails");
-            thumbDiv.innerHTML = "";
-
-            crateImage(endMax)
-            crateImage(endHQ)
-
-        }
-
-    } else {
-
-        alert('Insert Youtube link')
-
+      crateImage(endMax);
+      crateImage(endHQ);
     }
+  } else {
+    var thumbDiv = document.getElementById("thumbnails");
+    thumbDiv.innerHTML = "Invalid URL";
+    thumbDiv.className = "text-red-500 text-center";
+  }
 }
 
-/*function UrlExists(url) {
-    var http = new XMLHttpRequest();
-    http.open('GET', url, false);
-    http.setRequestHeader('Access-Control-Allow-Origin', '*');
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    http.send();
-
-    if (http.status != 404) {
-
-        var img = document.createElement('img');
-        img.src = url;
-        document.getElementById('thumbnails').appendChild(img);
-
-    } else {
-
-    }
-}*/
-
-
-function crateImage(url){
-    var img = document.createElement('img');
-    img.src = url;
-    document.getElementById('thumbnails').appendChild(img);
-}
-
-function showHelp(){
-
-    alert("Just paste the youtube link on the input box")
-
+function crateImage(url) {
+  var img = document.createElement("img");
+  img.className = "pb-5";
+  img.src = url;
+  document.getElementById("thumbnails").appendChild(img);
 }
